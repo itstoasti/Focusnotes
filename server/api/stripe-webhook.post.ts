12a0e6aa792249f9a8e5c29f610f1191e8493617
itsToasti <deanfields48@gmail.com>
2025-01-20 02:48:13 -0700

    Update Stripe API version to latest

--- server/api/stripe-webhook.post.ts
+++ server/api/stripe-webhook.post.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 import { serverSupabaseClient } from '#supabase/server';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-12-18.acacia',
 });
 
 export default defineEventHandler(async (event) => {
